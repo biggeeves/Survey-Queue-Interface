f8e93574b82fe801a31cae2662b9bcf9d49dd5f9
alee2 <alee2@bcchr.ca>
2019-05-01 13:49:51 -0700

    Version fixes.

--- index.php
+++ index.php
@@ -1,4 +1,6 @@
 <?php
 // Call the REDCap Connect file in the main "redcap" directory
+require_once APP_PATH_DOCROOT . "ProjectGeneral/header.php"; 
 $surveyQueueInterface = new \BCCHR\SurveyQueueInterface\SurveyQueueInterface();
 $surveyQueueSettings = $surveyQueueInterface->generate_user_interface();
+require_once APP_PATH_DOCROOT . "ProjectGeneral/footer.php";
